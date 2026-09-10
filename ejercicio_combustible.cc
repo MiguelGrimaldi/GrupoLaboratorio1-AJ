ca683345eb965ec78b971167427af8c746b7a375
Erick Alexander Ramos Mineros <00067926@uca.edu.sv>
2026-09-10 10:55:04 -0600

    feature: se agrego la funcion Consultar combustible

--- ejercicio_combustible.cc
+++ ejercicio_combustible.cc
@@ -1,16 +1,56 @@
 #include <iostream>
 
-void ConsumirCombustible(double *combustible, double litros);
+float tombustible = 0;
+float &CombuCambio = tombustible;
+float *combustible = &tombustible;
+
+void ConsultarCombustible();
+void ConsumirCombustible(float *combustible, float litros);
 
 int main()
 {
-    ConsumirCombustible(combustible,20.0);
+    int opciones = 0;
+    tombustible = 50;
+    while(opciones != 4){
+    std::cout << "Seleccione la opcion que desea" << "\n";
+    std::cout << "Opcion 1: Consultar Combustivo" << "\n";
+    std::cout << "Opcion 2: Repostar" << "\n";
+    std::cout << "Opcion 3: Consumir combustible" << "\n";
+    std::cout << "Opcion 4: Salir del programa" << "\n";
+    std::cin >> opciones;
+
+    switch(opciones){
+        case 1:
+        ConsultarCombustible();
+        break;
+
+        case 2:
+        
+        break;
+
+        case 3:
+        ConsumirCombustible(combustible,20.0);
+        break;
+
+
+
+        default: 
+        std::cout << "opcion no valia" << std::endl;
+        break;
+            
+    }
+}
+    
     std::cout << "Nivel actual de combustible: " << combustible << "\n";
 
     return 0;
 }
 
-void ConsumirCombustible(double *combustible, double litros)
+void ConsultarCombustible(){
+    std::cout << "La cantidad de combustible disponible es de " << CombuCambio << " Litros" << "\n";
+}
+
+void ConsumirCombustible(float *combustible, float litros)
 {
     if (litros < 0)
     {
